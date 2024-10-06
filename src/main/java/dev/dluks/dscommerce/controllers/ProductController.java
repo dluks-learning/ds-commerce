43853ae645d25a4f793c90298e1ebc5332481b8c
Diogo Oliveira <dluks82@gmail.com>
2024-10-05 23:15:30 -0300

    :sparkles: feat: Create entity classes

---
+++ src/main/java/dev/dluks/dscommerce/controllers/ProductController.java
@@ -0,0 +1,16 @@
+package dev.dluks.dscommerce.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public String getProducts() {
+        return "Products";
+    }
+
+}
---
+++ src/main/java/dev/dluks/dscommerce/models/OrderStatus.java
@@ -0,0 +1,9 @@
+package dev.dluks.dscommerce.models;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED;
+}
