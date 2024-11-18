9e792223ccb61c825e2004fc1bfa0100af1a5a9c
Diogo Oliveira <lucasdeoliveiradiogo@gmail.com>
2024-11-17 22:26:48 -0300

    :wrench: fix: Add getRoles` method to User class

--- src/main/java/dev/dluks/dscommerce/models/User.java
+++ src/main/java/dev/dluks/dscommerce/models/User.java
@@ -135,6 +135,10 @@ public class User implements UserDetails {
         roles.add(role);
     }
 
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
     public boolean hasRole(String roleName) {
         for (Role role : roles) {
             if (role.getAuthority().equals(roleName)) {
