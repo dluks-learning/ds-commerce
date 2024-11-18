24edef9dd2898e769b805afafd536838755392d6
Diogo Oliveira <lucasdeoliveiradiogo@gmail.com>
2024-11-17 22:27:33 -0300

    :sparkles: feat: Create `OrderDTO

---
+++ src/main/java/dev/dluks/dscommerce/dtos/OrderDTO.java
@@ -0,0 +1,63 @@
+package dev.dluks.dscommerce.dtos;
+
+import dev.dluks.dscommerce.models.Order;
+import dev.dluks.dscommerce.models.OrderStatus;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDTO {
+
+    private Long id;
+    private Instant moment;
+    private OrderStatus status;
+    private ClientDTO client;
+    private PaymentDTO payment;
+    private List<OrderItemDTO> items = new ArrayList<>();
+
+    public OrderDTO(Long id, Instant moment, OrderStatus status, ClientDTO client, PaymentDTO payment) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+        this.payment = payment;
+    }
+
+    public OrderDTO(Order entity) {
+        id = entity.getId();
+        moment = entity.getMoment();
+        status = entity.getStatus();
+        client = new ClientDTO(entity.getClient());
+        payment = (entity.getPayment() == null) ? null : new PaymentDTO(entity.getPayment());
+        entity.getItems().forEach(item -> items.add(new OrderItemDTO(item)));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public ClientDTO getClient() {
+        return client;
+    }
+
+    public PaymentDTO getPayment() {
+        return payment;
+    }
+
+    public List<OrderItemDTO> getItems() {
+        return items;
+    }
+
+    public Double getTotal() {
+        return items.stream().mapToDouble(OrderItemDTO::getSubTotal).sum();
+    }
+}
